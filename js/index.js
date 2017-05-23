47ebf24dcc530e5405d4b29939a5ff1b72a20355
callejerodeandalucia <cdau.ieca@juntadeandalucia.es>
2017-05-23 14:33:15 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -75,14 +75,18 @@ function geolocalizar(){
 			  cargarCategoria();
 		  };
 		  var errorFunction = function(){
-			  alert("Se ha producido un error al geolocalizar");
+			  alert("Compruebe que tiene activada la ubicación del dispositivo y que se encuentra cerca del municipio: Cuevas de San Marcos");
 			  loading(false);
 		  };
 		  loading(true);
-		  navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
-		} else {
+		  navigator.geolocation.getCurrentPosition(successFunction, errorFunction,{
+			maximumAge: 75000,
+			timeout: 7000
+		  });
+		  
+	} else {
 		  alert("El navegador utilizado no soporta la geolocalización");
-		}
+	}
 }
 
 function cargarCategoria(cat){
